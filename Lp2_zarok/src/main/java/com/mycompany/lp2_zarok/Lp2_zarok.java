d75fe49a69ffb41e26af0718cd9b9e8885e133ce
Katsuo Kumagai <108839865+SrZarok@users.noreply.github.com>
2023-03-31 21:31:25 -0400

    Add files via upload

---
+++ Lp2_zarok/src/main/java/com/mycompany/lp2_zarok/Lp2_zarok.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lp2_zarok;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Lp2_zarok {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
